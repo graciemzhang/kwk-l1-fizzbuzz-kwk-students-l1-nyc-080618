085eb507b62e996d8277d788d764b05b9ec77e69
graciemzhang <gracie.zhang@lps-students.org>
2018-08-07 19:48:43 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,13 @@
 def fizzbuzz(num)
 
-if num%3 == 0
-  if num%5 == 0
-    return "FizzBuzz"
-  else
-    return "Fizz"
+if num%3 == 0 && num%5 == 0
+  return "FizzBuzz"
   
 elsif num%5 == 0
   return "Buzz"
+
+elsif num%3 == 0
+  return "FizzBuzz"
   
 end
 end
